a1099a7876d4d0f3f6f9473b8eb083b3f93fa82b
11aghayan <11.aghayan.99@gmail.com>
2024-12-19 02:49:58 +0400

    russian context updated

--- src/app/ui/home/Top_Bar.tsx
+++ src/app/ui/home/Top_Bar.tsx
@@ -7,7 +7,7 @@ export default function Top_Bar() {
   
   return (
     <section className="w-full">
-      <div className="w-full py-1 px-2 md:py-2 text-sm flex gap-10 items-center justify-between">
+      <div className="w-full py-1 px-2 md:py-2 flex gap-10 items-center justify-between">
         <SidebarTrigger />
         <Search className="flex-1 flex flex-shrink-0 justify-end sm:justify-center" />
       </div>
