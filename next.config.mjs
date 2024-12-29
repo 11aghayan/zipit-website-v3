ba7e50215531b88bb775f43a661e4c1824954acb
11aghayan <11.aghayan.99@gmail.com>
2024-12-29 15:14:49 +0400

    eslint rules changed

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
         hostname: process.env.NEXT_PUBLIC_API_HOST
       }
     ]
-  }
+  },
+  eslint: {
+    dirs: ["app", "lib", "actions"]
+  },
 };
 
 export default nextConfig;
